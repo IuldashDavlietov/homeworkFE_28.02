44225cca0a8ba4f4cb5d4f51bbd70fe9d27220fe
IuldashDavlietov <118936839+IuldashDavlietov@users.noreply.github.com>
2023-03-11 10:43:32 +0100

    Add files via upload

---
+++ HomeworkFE_28.02/scripts.js
@@ -0,0 +1,50 @@
+//Task 1
+
+// let arr = {
+//   ru:[‘голубой‘, ‘красный‘, ‘зеленый’],
+//   en:[‘blue’, ‘red’, ‘green’],
+// };
+// console.log(arr.ru[0])
+
+
+// Task 2
+
+// const obj ={
+//     Nikolay: 200,
+//     Vasiliy: 300,
+//     Petr: 400,
+// }
+// for (let key in obj) {
+//     alert(`${key} - salary ${obj[key]}`)
+// }
+
+
+// Task 3
+
+// let obj = {
+//     js: ["jQuery", "Angular"],
+//     php: "hello",
+//     css: "world"
+// };
+// console.log(`${obj.js[0]}`);
+
+// Task 4
+
+// let array = {
+//     "ru":["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"],
+//     "en":["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+// };
+// console.log(`${array.ru[0]} and ${array.en[2]}`)
+
+// Task 5
+
+// let object = {
+//     first: "Monday",
+//     second: "Tuesday",
+//     third: "Wednesday",
+//     fourth: "Thursday",
+//     fifth: "Friday",
+//     sixth: "Saturday",
+//     seventh: "Sunday"
+// };
+// console.log(object.fourth);
